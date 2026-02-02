05650745b021907c57d3c3d1c7e03d9ee9d7e7c6
Govind v Kartha <knvgovind@gmail.com>
2026-02-02 05:34:38 +0530

    Refactor: Rename orchestrators/ to workflows/

---
+++ workflows/__init__.py
@@ -0,0 +1,11 @@
+"""
+Workflows Package
+
+Contains the main workflow modules for encryption and decryption pipelines.
+
+Modules:
+- encrypt.py: Encryption workflow (12-step pipeline)
+- decrypt.py: Decryption workflow (13-step pipeline)
+
+Both workflows control the flow of data through independent engines in /engines/.
+"""
